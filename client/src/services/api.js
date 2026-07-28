b366f2dcf661a7a254bfcef3e503ee8a44950747
Ankit Kumar <ankitrishu2345@gmail.com>
2026-07-28 08:49:19 +0530

    Prepare project for deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,20 +1,29 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "http://localhost:5000/api",
+
   headers: {
-    "Content-Type": "application/json",
+    "Content-Type":
+      "application/json",
   },
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+api.interceptors.request.use(
+  (config) => {
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    const token =
+      localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization =
+        `Bearer ${token}`;
+    }
+
+    return config;
   }
-
-  return config;
-});
+);
 
 export default api;
